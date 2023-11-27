650a944d0aaaca2c02038e930e79cb5a9307c133
Maxy Barrios <licjavierbarrios@hotmail.com>
2023-11-27 12:48:25 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,98 @@
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
+import Header from "./src/components/Header";
+import Timer from "./src/components/Timer";
+
+import { Audio } from "expo-av";
+
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
+
+export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" || "SHORT" || "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking(prev => !prev);
+      setTime(isWorking ? 5 * 60 : 25 * 60);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  function handleStartStop() {
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/click.mp3")
+    );
+    await sound.playAsync();
+  }
+
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 30,
+        }}
+      >
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        <TouchableOpacity style={styles.button} onPress={handleStartStop}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "#333333",
+    padding: 15,
+    borderRadius: 15,
+    alignItems: "center",
+    marginTop: 30,
+  },
+});
---
+++ README.md
@@ -0,0 +1,31 @@
+# Pomodoro App en React Native
+
+Esta es una simple aplicación Pomodoro desarrollada en React Native para ayudarte a aprender y practicar los conceptos básicos de la tecnología.
+
+## Descripción
+
+La técnica Pomodoro es un método de gestión del tiempo desarrollado por Francesco Cirillo en la década de 1980. La técnica utiliza un temporizador para dividir el trabajo en intervalos, tradicionalmente de 25 minutos de duración, separados por descansos cortos. Esta aplicación implementa esta técnica para ayudarte a mejorar tu productividad.
+
+## Funcionalidades
+
+- Temporizador Pomodoro ajustable
+- Contador de ciclos completados
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local.
+   ```
+   git clone https://github.com/licjavierbarrios/react-native-pomodoro.git
+   ```
+2. Navega al directorio del proyecto.
+   ```
+   cd pomodoro-app
+   ```
+3. Instala las dependencias.
+   ```
+   npm install
+   ```
+4. Inicia la aplicación.
+   ```
+   npx expo start
+   ```
